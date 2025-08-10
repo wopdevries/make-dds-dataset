3868cab8d0bf4b845519738cac919f28f95bc849
wopdevries <wop@live.nl>
2025-08-10 12:41:16 +0200

    Fix DDS v2.8.4 build with ABsearch.cpp and PBN.cpp patches, update PBN in scripts

--- test_dds.py
+++ test_dds.py
@@ -29,7 +29,8 @@ class DDTable(ctypes.Structure):
 lib.CalcDDtablePBN.argtypes = [dealPBN, ctypes.POINTER(DDTable)]
 lib.CalcDDtablePBN.restype = ctypes.c_int
 
-pbn = "N:A2.KT.T763.AKQ87 KQ873.J54.Q85.T9 T5.Q632.KJ94.654 J964.A987.A2.J32"
+# Test with a valid PBN (13 cards per hand)
+pbn = "N:AKQJ.T98.AK.5432 T98.AK.QJT.9876 432.QJT.9876.AKQ 765.65432.5432.JT"
 pbn_bytes = pbn.encode('utf-8')
 deal = dealPBN()
 deal.trump = 0
